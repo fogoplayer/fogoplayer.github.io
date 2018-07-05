7a2c8cb61ec8cc9373f95a7bca4a1130b2a81453
Zarin Loosli <fogoplayer@gmail.com>
2018-07-05 15:15:24 +0000

    Swtiched to reveal cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
-import { projects } from './projects.js';
+import {
+    projects
+}
+from './projects.js';
 
 //This is the service worker with the Cache-first network
 
@@ -17,35 +20,48 @@ if (navigator.serviceWorker.controller) {
 
 const body = document.getElementById('body');
 projects.forEach(project => {
-    console.log(project);
-    
+
     const column = document.createElement('div');
-    column.className='col s12 m6 l3';
-    
+    column.className = "col s12 m6 l3";
+
     const card = document.createElement('div');
-    card.className="card blue-grey darken-1";
+    card.className = "card blue-grey darken-1";
+
+        const image = document.createElement('div');
+        image.className = "card-image waves-effect waves-block waves-light";
+        image.innerHTML = `<img class="activator" src="assets/images/zarin.svg" />`;
     
-    const content = document.createElement('div');
-    content.className ="card-content white-text";
+        const content = document.createElement('div');
+        content.className = "card-content white-text";
     
-    const title = document.createElement('span');
-    title.className="card-title";
-    title.innerHTML = 'Card Title';
+            const title = document.createElement('span');
+            title.className = "card-title";
+            title.innerHTML = ` ${ project.name }<i class="material-icons right">more_vert</i>`;
+            
+            content.appendChild(title);
     
-    const description = document.createElement('p');
-    description.innerHTML = 'I am a very simple card. I am good at containing small bits of information. I am convenient because I require little markup to use effectively.';
+        const description = document.createElement('div');
+        description.className="card-reveal"
+        
+            const revealTitle = document.createElement('span');
+            revealTitle.className = "card-title";
+            revealTitle.innerHTML = `${ project.name }<i class="material-icons right">close</i>`;
+            
+            const p = document.createTextNode(project.description);
+            
+            description.appendChild(revealTitle);
+            description.appendChild(p);
     
-    const actions = document.createElement('div');
-    actions.className="card-action";
-    actions.innerHTML='<a href="#">This is a link</a><a href="#">This is a link</a>';
-    
-    content.appendChild(title);
-    content.appendChild(description);
-    
-    card.appendChild(content);
-    card.appendChild(actions);
-    
+        const actions = document.createElement('div');
+        actions.className = "card-action";
+        actions.innerHTML = `<a href="${ project.url }">View project</a><a href="#">This is a link</a>`;
+
+        card.appendChild(image);
+        card.appendChild(content);
+        card.appendChild(actions);
+        card.appendChild(description);
+
     column.appendChild(card);
-    
+
     body.appendChild(column);
 });

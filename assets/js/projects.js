08453be85426138ea5765b9063e3e667a29f5d22
fogoplayer <zarinloosli@gmail.com>
2021-05-18 08:35:20 -0600

    Updated with new projects 18 May 21

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,129 +1,177 @@
-export const projects = [{
-    name: 'sCalc',
-    url: 'https://fogoplayer.github.io/sCalc',
-    startDate: '2014',
-    endDate: 'March 31, 2016',
-    lessons: 'JQuery, DOM manipulation, Code Splitting, Github Pages',
-    description: 'The precursor to Coyote Calculator, sCalc was my first published web app: a calculator that performed common but time-consuming functions like factoring.',
-}, {
-    name: 'Coyote Calculator',
-    url: 'https://fogoplayer.github.io/CoyoteCalculator',
-    startDate: 'July 8 2016',
-    endDate: 'Oct 4 2016',
-    lessons: '',
-    description: 'A refined version of sCalc, Coyote brought a refined design based on Material Design and new features like basic algebra.',
-}, {
-    name: 'Hamilton Karaoke',
-    url: 'https://fogoplayer.github.io/karaoke/hamilton.html',
-    startDate: 'Oct 26, 2016',
-    endDate: 'Nov 1, 2016',
-    lessons: 'Single Page Applications, YouTube API, @media tags',
-    description: 'A friend challenged me to create a karaoke machine for the Broadway musical Hamilton, so I coded a website that automatically generated a homepage and lyrics sheets from a JS object. I would only later learn that JSON had been invented for just this purpose. While I didn\'t add every feature I had planned, I was quite proud of how it turned out.',
-}, {
-    name: 'Sub-cipher',
-    url: 'https://fogoplayer.github.io/sub-cipher',
-    startDate: 'August 31, 2017',
-    endDate: 'Sept 1, 2017',
-    lessons: 'localStorage API',
-    description: 'A quick little two-day hack-together to explore the localStorage API. I\'ve always loved codes and ciphers, and an automated way to encrypt and decrypt messages sounded like a lot of fun. It\'s also the first instance of me using dashes to name a project.',
-}, {
-    name: 'Console',
-    url: 'https://fogoplayer.github.io/console',
-    startDate: 'Sept 12 2017',
-    endDate: 'Sept 26 2017',
-    lessons: '',
-    description: 'An incredibly simple REPL because the DevTools console was blocked on school computers. Since discovering other, more fully-featured REPLs (mostly by virtue of learning what a REPL is, this project has been abandoned.',
-}, {
-    name: 'HTML Template',
-    url: 'https://fogoplayer.github.io/html-template',
-    startDate: 'Jan 27, 2018',
-    endDate: 'July 5, 2018',
-    lessons: '',
-    description: 'My boilerplate for building web apps.',
-}, {
-    name: 'CYOA',
-    longName: 'Choose Your Own Adventure',
-    url: 'https://fogoplayer.github.io/Early-Projects/Choose%20Your%20Own%20Adventure.html',
-    startDate: '2012',
-    endDate: '2013',
-    lessons: '',
-    description: 'One of three "early projects" that I\'ve put on Github for... posterity, I guess. I didn\'t know about DOM manipulation, so the whole thing is prompts and confirm dialogs.',
-}, {
-    name: 'JARVIS',
-    url: 'https://fogoplayer.github.io/Early-Projects/JARVIS.html',
-    startDate: '2012',
-    endDate: '2013',
-    lessons: '',
-    description: 'One of three "early projects" that I\'ve put on Github for... posterity, I guess. I didn\'t know about DOM manipulation, so the whole thing is prompts and confirm dialogs.',
-}, {
-    name: 'Spirit Animal Quiz',
-    url: 'https://fogoplayer.github.io/Early-Projects/Quiz.html',
-    startDate: '2012',
-    endDate: '2013',
-    lessons: '',
-    description: 'One of three "early projects" that I\'ve put on Github for... posterity, I guess. I didn\'t know about DOM manipulation, so the whole thing is prompts and confirm dialogs.',
-}, {
-    name: 'LDSYouth',
-    url: 'https://fogoplayer.github.io/LDSYouth',
-    startDate: 'Sept 28, 2017',
-    endDate: 'March 25 2018',
-    lessons: 'MaterializeCSS',
-    description: 'My church has these programs for teenagers to facilitate personal growth, and each has a handbook that you read through and fill out. The handbook is available online and in the Church\'s app, but not in an interactive way. I saw room for improvement, so I built my own.',
-}, {
-    name: 'S.W.A.R.M.',
-    url: 'https://fogoplayer.github.io/swarm',
-    startDate: 'May 31 2017',
-    endDate: 'May 22 2018',
-    lessons: 'Service Worker, Git CLI, Python, Django, Web Geolocation, Heroku, Algorithm Design, CORS',
-    description: 'For my high school capstone engineering project, I led a team of two other students to build an app that would coordinate parking lot traffic to improve efficiency. Due to some issues with CORS headers, we ran out of time to implement a fully-working app, but we had a working algorithm, app, and communications protocol. What you see if you follow the link is a demo we made that generates fake data. If you download the source code and run Server/runner.py, you can see the test suite for the backend algorithm.',
-}, {
-    name: 'Web-Mirror',
-    url: 'https://fogoplayer.github.io/web-mirror',
-    startDate: 'June 9 2018',
-    endDate: 'June 26 2018',
-    lessons: 'getUserMedia API, visibility API, PWABuilder',
-    description: 'I wanted a mirror app, but I wasn\'t happy with any of the ones I found. Native apps were too big, web apps were not fullscreen or offline-capable. So I built my own in an evening (and then made a few tweaks later).',
-}, {
-    name: 'LDS Youth (React)',
-    url: 'https://ldsyouth-195000.firebaseapp.com/',
-    startDate: 'July 1 2018',
-    endDate: 'PRESENT',
-    lessons: 'ReactJS, react-router, Firebase, NPM, Webpack, http-server',
-    description: 'Pretty much as soon as LDSYouth had hit minimum viable product, I knew it would need a rewrite. I spent too much time going through each file tweaking things. I wanted to rebuild it with an app shell model, and with Firebase integration to allow for device sync and notifications.',
-}, {
-    name: 'The Legend of Chica',
-    longName: 'The Legend of Chica: Golden Guardian',
-    url: 'https://fogoplayer.github.io/legend-of-chica/',
-    startDate: 'April 6, 2018',
-    endDate: 'Aug 17, 2018',
-    lessons: 'Phaser 3, Arrow functions, Modules, Classes',
-    description: 'My friend approached me and asked if I\'d be willing to help her code a game she was writing. Without hesitation, I said yes, though she later lost interest, leaving the project incomplete.',
-}, {
-    name: 'Portfolio',
-    url: 'https://fogoplayer.github.io/',
-    startDate: 'July 4, 2018',
-    endDate: 'July 12, 2018',
-    lessons: '',
-    description: 'I finally got around to building a personal portfolio',
-}, {
-    name: 'Blockeddd',
-    url: 'https://launch.playcanvas.com/611199?debug=true',
-    startDate: 'April 25, 2018',
-    endDate: 'May 25, 2018',
-    lessons: 'PlayCanvas, Sort-search algorithms, Prototyping',
-    description: 'My final project for my AP Computer Science (Java) class, Blockeddd is a 3D third-person shooter game. Four other students and I, many of whom didn\'t know JavaScript at the beginning of the project, worked for a month to build it.',
-}, {
-    name: 'futuristic-materialize',
-    url: 'https://fogoplayer.github.io/materialize',
-    startDate: 'July 5, 2018',
-    endDate: 'July 11, 2018',
-    lessons: 'CSS Variables and Combinators, for-in loops, Markdown, Github releases, Git reset, jsDelivr',
-    description: 'This project began as a few tweaks to my own materialize.css folder to make a dark theme for my personal site, but it quickly spiraled from there. I used CSS variables to create a fork of MaterializeCSS with an epic sci-fi style interface.',
-}, {
-    name: 'ZTasks',
-    url: '',
-    startDate: 'Sept 7, 2018',
-    endDate: 'PRESENT',
-    lessons: 'CSS Cursors',
-    description: 'I had tried every todo app I could find, and I was unsatisfied with all of them... so I built my own. It was also an opportunity to build a real-world application using futuristic-materialize, and I\'m happy to say that it held up well.',
-}];
+export const projects = [
+  {
+    name: "sCalc",
+    url: "https://fogoplayer.github.io/sCalc",
+    startDate: "2014",
+    endDate: "March 31, 2016",
+    lessons: "JQuery, DOM manipulation, Code Splitting, Github Pages",
+    description:
+      "The precursor to Coyote Calculator, sCalc was my first published web app: a calculator that performed common but time-consuming functions like factoring."
+  },
+  {
+    name: "Coyote Calculator",
+    url: "https://fogoplayer.github.io/CoyoteCalculator",
+    startDate: "July 8 2016",
+    endDate: "Oct 4 2016",
+    lessons: "",
+    description:
+      "A refined version of sCalc, Coyote brought a refined design based on Material Design and new features like basic algebra."
+  },
+  {
+    name: "Hamilton Karaoke",
+    url: "https://fogoplayer.github.io/karaoke/hamilton.html",
+    startDate: "Oct 26, 2016",
+    endDate: "Nov 1, 2016",
+    lessons: "Single Page Applications, YouTube API, @media tags",
+    description:
+      "A friend challenged me to create a karaoke machine for the Broadway musical Hamilton, so I coded a website that automatically generated a homepage and lyrics sheets from a JS object. I would only later learn that JSON had been invented for just this purpose. While I didn't add every feature I had planned, I was quite proud of how it turned out."
+  },
+  {
+    name: "Sub-cipher",
+    url: "https://fogoplayer.github.io/sub-cipher",
+    startDate: "August 31, 2017",
+    endDate: "Sept 1, 2017",
+    lessons: "localStorage API",
+    description:
+      "A quick little two-day hack-together to explore the localStorage API. I've always loved codes and ciphers, and an automated way to encrypt and decrypt messages sounded like a lot of fun. It's also the first instance of me using dashes to name a project."
+  },
+  {
+    name: "Console",
+    url: "https://fogoplayer.github.io/console",
+    startDate: "Sept 12 2017",
+    endDate: "Sept 26 2017",
+    lessons: "",
+    description:
+      "An incredibly simple REPL because the DevTools console was blocked on school computers. Since discovering other, more fully-featured REPLs (mostly by virtue of learning what a REPL is, this project has been abandoned."
+  },
+  {
+    name: "HTML Template",
+    url: "https://fogoplayer.github.io/html-template",
+    startDate: "Jan 27, 2018",
+    endDate: "July 5, 2018",
+    lessons: "",
+    description: "My boilerplate for building web apps."
+  },
+  {
+    name: "CYOA",
+    longName: "Choose Your Own Adventure",
+    url:
+      "https://fogoplayer.github.io/Early-Projects/Choose%20Your%20Own%20Adventure.html",
+    startDate: "2012",
+    endDate: "2013",
+    lessons: "",
+    description:
+      "One of three \"early projects\" that I've put on Github for... posterity, I guess. I didn't know about DOM manipulation, so the whole thing is prompts and confirm dialogs."
+  },
+  {
+    name: "JARVIS",
+    url: "https://fogoplayer.github.io/Early-Projects/JARVIS.html",
+    startDate: "2012",
+    endDate: "2013",
+    lessons: "",
+    description:
+      "One of three \"early projects\" that I've put on Github for... posterity, I guess. I didn't know about DOM manipulation, so the whole thing is prompts and confirm dialogs."
+  },
+  {
+    name: "Spirit Animal Quiz",
+    url: "https://fogoplayer.github.io/Early-Projects/Quiz.html",
+    startDate: "2012",
+    endDate: "2013",
+    lessons: "",
+    description:
+      "One of three \"early projects\" that I've put on Github for... posterity, I guess. I didn't know about DOM manipulation, so the whole thing is prompts and confirm dialogs."
+  },
+  {
+    name: "LDSYouth",
+    url: "https://fogoplayer.github.io/LDSYouth",
+    startDate: "Sept 28, 2017",
+    endDate: "March 25 2018",
+    lessons: "MaterializeCSS",
+    description:
+      "My church has these programs for teenagers to facilitate personal growth, and each has a handbook that you read through and fill out. The handbook is available online and in the Church's app, but not in an interactive way. I saw room for improvement, so I built my own."
+  },
+  {
+    name: "S.W.A.R.M.",
+    url: "https://fogoplayer.github.io/swarm",
+    startDate: "May 31 2017",
+    endDate: "May 22 2018",
+    lessons:
+      "Service Worker, Git CLI, Python, Django, Web Geolocation, Heroku, Algorithm Design, CORS",
+    description:
+      "For my high school capstone engineering project, I led a team of two other students to build an app that would coordinate parking lot traffic to improve efficiency. Due to some issues with CORS headers, we ran out of time to implement a fully-working app, but we had a working algorithm, app, and communications protocol. What you see if you follow the link is a demo we made that generates fake data. If you download the source code and run Server/runner.py, you can see the test suite for the backend algorithm."
+  },
+  {
+    name: "Web-Mirror",
+    url: "https://fogoplayer.github.io/web-mirror",
+    startDate: "June 9 2018",
+    endDate: "June 26 2018",
+    lessons: "getUserMedia API, visibility API, PWABuilder",
+    description:
+      "I wanted a mirror app, but I wasn't happy with any of the ones I found. Native apps were too big, web apps were not fullscreen or offline-capable. So I built my own in an evening (and then made a few tweaks later)."
+  },
+  {
+    name: "LDS Youth (React)",
+    url: "https://ldsyouth-195000.firebaseapp.com/",
+    startDate: "July 1 2018",
+    endDate: "Sept 6, 2018",
+    lessons: "ReactJS, react-router, Firebase, NPM, Webpack, http-server",
+    description:
+      "Pretty much as soon as LDSYouth had hit minimum viable product, I knew it would need a rewrite. I spent too much time going through each file tweaking things. I wanted to rebuild it with an app shell model, and with Firebase integration to allow for device sync and notifications."
+  },
+  {
+    name: "The Legend of Chica",
+    longName: "The Legend of Chica: Golden Guardian",
+    url: "https://fogoplayer.github.io/legend-of-chica/",
+    startDate: "April 6, 2018",
+    endDate: "Aug 17, 2018",
+    lessons: "Phaser 3, Arrow functions, Modules, Classes",
+    description:
+      "My friend approached me and asked if I'd be willing to help her code a game she was writing. Without hesitation, I said yes, though she later lost interest, leaving the project incomplete."
+  },
+  {
+    name: "Portfolio",
+    url: "https://fogoplayer.github.io/",
+    startDate: "July 4, 2018",
+    endDate: "July 12, 2018",
+    lessons: "",
+    description: "I finally got around to building a personal portfolio"
+  },
+  {
+    name: "Blockeddd",
+    url: "https://launch.playcanvas.com/611199?debug=true",
+    startDate: "April 25, 2018",
+    endDate: "May 25, 2018",
+    lessons: "PlayCanvas, Sort-search algorithms, Prototyping",
+    description:
+      "My final project for my AP Computer Science (Java) class, Blockeddd is a 3D third-person shooter game. Four other students and I, many of whom didn't know JavaScript at the beginning of the project, worked for a month to build it."
+  },
+  {
+    name: "futuristic-materialize",
+    url: "https://fogoplayer.github.io/materialize",
+    startDate: "July 5, 2018",
+    endDate: "July 11, 2018",
+    lessons:
+      "CSS Variables and Combinators, for-in loops, Markdown, Github releases, Git reset, jsDelivr",
+    description:
+      "This project began as a few tweaks to my own materialize.css folder to make a dark theme for my personal site, but it quickly spiraled from there. I used CSS variables to create a fork of MaterializeCSS with an epic sci-fi style interface."
+  },
+  {
+    name: "ZTasks",
+    url: "https://github.com/fogoplayer/ztasks",
+    startDate: "Sept 7, 2018",
+    endDate: "Sept 29, 2018",
+    lessons: "CSS Cursors",
+    description:
+      "I had tried every todo app I could find, and I was unsatisfied with all of them... so I built my own. It was also an opportunity to build a real-world application using futuristic-materialize, and I'm happy to say that it held up well.\n\nDevelopment was suspended when I left on my mission, but I hope to return to the project soon."
+  },
+  {
+    name: "Pens",
+    url: "https://fogoplayer.github.io/pens",
+    startDate: "Sept 18, 2020",
+    endDate: "May 18, 2021",
+    lessons:
+      "Bash Scripting, CSS Transform, CSS Transition, Google Apps Script, Bookmarklets",
+    description:
+      "This is a collection of quick-and-dirty projects that I've made over the years to learn new concepts or solve a particular problem. They're not supposed to be finished products--they're supposed to solve a very narrow problem."
+  }
+];

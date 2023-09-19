85a93499984558feaa0a8071acad973b331ea952
Zarin Loosli <zarinloosli@gmail.com>
2023-09-19 15:59:31 -0600

    Add sine nire orihects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -6,7 +6,7 @@ export const projects = [
     endDate: "March 31, 2016",
     lessons: "JQuery, DOM manipulation, Code Splitting, Github Pages",
     description:
-      "The precursor to Coyote Calculator, sCalc was my first published web app: a calculator that performed common but time-consuming functions like factoring."
+      "The precursor to Coyote Calculator, sCalc was my first published web app: a calculator that performed common but time-consuming functions like factoring.",
   },
   {
     name: "Coyote Calculator",
@@ -15,7 +15,7 @@ export const projects = [
     endDate: "Oct 4 2016",
     lessons: "",
     description:
-      "A refined version of sCalc, Coyote brought a refined design based on Material Design and new features like basic algebra."
+      "A refined version of sCalc, Coyote brought a refined design based on Material Design and new features like basic algebra.",
   },
   {
     name: "Hamilton Karaoke",
@@ -24,7 +24,7 @@ export const projects = [
     endDate: "Nov 1, 2016",
     lessons: "Single Page Applications, YouTube API, @media tags",
     description:
-      "A friend challenged me to create a karaoke machine for the Broadway musical Hamilton, so I coded a website that automatically generated a homepage and lyrics sheets from a JS object. I would only later learn that JSON had been invented for just this purpose. While I didn't add every feature I had planned, I was quite proud of how it turned out."
+      "A friend challenged me to create a karaoke machine for the Broadway musical Hamilton, so I coded a website that automatically generated a homepage and lyrics sheets from a JS object. I would only later learn that JSON had been invented for just this purpose. While I didn't add every feature I had planned, I was quite proud of how it turned out.",
   },
   {
     name: "Sub-cipher",
@@ -33,7 +33,7 @@ export const projects = [
     endDate: "Sept 1, 2017",
     lessons: "localStorage API",
     description:
-      "A quick little two-day hack-together to explore the localStorage API. I've always loved codes and ciphers, and an automated way to encrypt and decrypt messages sounded like a lot of fun. It's also the first instance of me using dashes to name a project."
+      "A quick little two-day hack-together to explore the localStorage API. I've always loved codes and ciphers, and an automated way to encrypt and decrypt messages sounded like a lot of fun. It's also the first instance of me using dashes to name a project.",
   },
   {
     name: "Console",
@@ -42,7 +42,7 @@ export const projects = [
     endDate: "Sept 26 2017",
     lessons: "",
     description:
-      "An incredibly simple REPL because the DevTools console was blocked on school computers. Since discovering other, more fully-featured REPLs (mostly by virtue of learning what a REPL is, this project has been abandoned."
+      "An incredibly simple REPL because the DevTools console was blocked on school computers. Since discovering other, more fully-featured REPLs (mostly by virtue of learning what a REPL is, this project has been abandoned.",
   },
   {
     name: "HTML Template",
@@ -50,18 +50,17 @@ export const projects = [
     startDate: "Jan 27, 2018",
     endDate: "July 5, 2018",
     lessons: "",
-    description: "My boilerplate for building web apps."
+    description: "My boilerplate for building web apps.",
   },
   {
     name: "CYOA",
     longName: "Choose Your Own Adventure",
-    url:
-      "https://fogoplayer.github.io/Early-Projects/Choose%20Your%20Own%20Adventure.html",
+    url: "https://fogoplayer.github.io/Early-Projects/Choose%20Your%20Own%20Adventure.html",
     startDate: "2012",
     endDate: "2013",
     lessons: "",
     description:
-      "One of three \"early projects\" that I've put on Github for... posterity, I guess. I didn't know about DOM manipulation, so the whole thing is prompts and confirm dialogs."
+      "One of three \"early projects\" that I've put on Github for... posterity, I guess. I didn't know about DOM manipulation, so the whole thing is prompts and confirm dialogs.",
   },
   {
     name: "JARVIS",
@@ -70,7 +69,7 @@ export const projects = [
     endDate: "2013",
     lessons: "",
     description:
-      "One of three \"early projects\" that I've put on Github for... posterity, I guess. I didn't know about DOM manipulation, so the whole thing is prompts and confirm dialogs."
+      "One of three \"early projects\" that I've put on Github for... posterity, I guess. I didn't know about DOM manipulation, so the whole thing is prompts and confirm dialogs.",
   },
   {
     name: "Spirit Animal Quiz",
@@ -79,7 +78,7 @@ export const projects = [
     endDate: "2013",
     lessons: "",
     description:
-      "One of three \"early projects\" that I've put on Github for... posterity, I guess. I didn't know about DOM manipulation, so the whole thing is prompts and confirm dialogs."
+      "One of three \"early projects\" that I've put on Github for... posterity, I guess. I didn't know about DOM manipulation, so the whole thing is prompts and confirm dialogs.",
   },
   {
     name: "LDSYouth",
@@ -88,7 +87,7 @@ export const projects = [
     endDate: "March 25 2018",
     lessons: "MaterializeCSS",
     description:
-      "My church has these programs for teenagers to facilitate personal growth, and each has a handbook that you read through and fill out. The handbook is available online and in the Church's app, but not in an interactive way. I saw room for improvement, so I built my own."
+      "My church has these programs for teenagers to facilitate personal growth, and each has a handbook that you read through and fill out. The handbook is available online and in the Church's app, but not in an interactive way. I saw room for improvement, so I built my own.",
   },
   {
     name: "S.W.A.R.M.",
@@ -98,7 +97,7 @@ export const projects = [
     lessons:
       "Service Worker, Git CLI, Python, Django, Web Geolocation, Heroku, Algorithm Design, CORS",
     description:
-      "For my high school capstone engineering project, I led a team of two other students to build an app that would coordinate parking lot traffic to improve efficiency. Due to some issues with CORS headers, we ran out of time to implement a fully-working app, but we had a working algorithm, app, and communications protocol. What you see if you follow the link is a demo we made that generates fake data. If you download the source code and run Server/runner.py, you can see the test suite for the backend algorithm."
+      "For my high school capstone engineering project, I led a team of two other students to build an app that would coordinate parking lot traffic to improve efficiency. Due to some issues with CORS headers, we ran out of time to implement a fully-working app, but we had a working algorithm, app, and communications protocol. What you see if you follow the link is a demo we made that generates fake data. If you download the source code and run Server/runner.py, you can see the test suite for the backend algorithm.",
   },
   {
     name: "Web-Mirror",
@@ -107,7 +106,7 @@ export const projects = [
     endDate: "June 26 2018",
     lessons: "getUserMedia API, visibility API, PWABuilder",
     description:
-      "I wanted a mirror app, but I wasn't happy with any of the ones I found. Native apps were too big, web apps were not fullscreen or offline-capable. So I built my own in an evening (and then made a few tweaks later)."
+      "I wanted a mirror app, but I wasn't happy with any of the ones I found. Native apps were too big, web apps were not fullscreen or offline-capable. So I built my own in an evening (and then made a few tweaks later).",
   },
   {
     name: "LDS Youth (React)",
@@ -116,7 +115,7 @@ export const projects = [
     endDate: "Sept 6, 2018",
     lessons: "ReactJS, react-router, Firebase, NPM, Webpack, http-server",
     description:
-      "Pretty much as soon as LDSYouth had hit minimum viable product, I knew it would need a rewrite. I spent too much time going through each file tweaking things. I wanted to rebuild it with an app shell model, and with Firebase integration to allow for device sync and notifications."
+      "Pretty much as soon as LDSYouth had hit minimum viable product, I knew it would need a rewrite. I spent too much time going through each file tweaking things. I wanted to rebuild it with an app shell model, and with Firebase integration to allow for device sync and notifications.",
   },
   {
     name: "The Legend of Chica",
@@ -126,7 +125,7 @@ export const projects = [
     endDate: "Aug 17, 2018",
     lessons: "Phaser 3, Arrow functions, Modules, Classes",
     description:
-      "My friend approached me and asked if I'd be willing to help her code a game she was writing. Without hesitation, I said yes, though she later lost interest, leaving the project incomplete."
+      "My friend approached me and asked if I'd be willing to help her code a game she was writing. Without hesitation, I said yes, though she later lost interest, leaving the project incomplete.",
   },
   {
     name: "Portfolio",
@@ -134,7 +133,7 @@ export const projects = [
     startDate: "July 4, 2018",
     endDate: "July 12, 2018",
     lessons: "",
-    description: "I finally got around to building a personal portfolio"
+    description: "I finally got around to building a personal portfolio",
   },
   {
     name: "Blockeddd",
@@ -143,7 +142,7 @@ export const projects = [
     endDate: "May 25, 2018",
     lessons: "PlayCanvas, Sort-search algorithms, Prototyping",
     description:
-      "My final project for my AP Computer Science (Java) class, Blockeddd is a 3D third-person shooter game. Four other students and I, many of whom didn't know JavaScript at the beginning of the project, worked for a month to build it."
+      "My final project for my AP Computer Science (Java) class, Blockeddd is a 3D third-person shooter game. Four other students and I, many of whom didn't know JavaScript at the beginning of the project, worked for a month to build it.",
   },
   {
     name: "futuristic-materialize",
@@ -153,7 +152,7 @@ export const projects = [
     lessons:
       "CSS Variables and Combinators, for-in loops, Markdown, Github releases, Git reset, jsDelivr",
     description:
-      "This project began as a few tweaks to my own materialize.css folder to make a dark theme for my personal site, but it quickly spiraled from there. I used CSS variables to create a fork of MaterializeCSS with an epic sci-fi style interface."
+      "This project began as a few tweaks to my own materialize.css folder to make a dark theme for my personal site, but it quickly spiraled from there. I used CSS variables to create a fork of MaterializeCSS with an epic sci-fi style interface.",
   },
   {
     name: "ZTasks",
@@ -162,16 +161,52 @@ export const projects = [
     endDate: "Sept 29, 2018",
     lessons: "CSS Cursors",
     description:
-      "I had tried every todo app I could find, and I was unsatisfied with all of them... so I built my own. It was also an opportunity to build a real-world application using futuristic-materialize, and I'm happy to say that it held up well.\n\nDevelopment was suspended when I left on my mission, but I hope to return to the project soon."
+      "I had tried every todo app I could find, and I was unsatisfied with all of them... so I built my own. It was also an opportunity to build a real-world application using futuristic-materialize, and I'm happy to say that it held up well.\n\nDevelopment was suspended when I left on my mission, but I hope to return to the project soon.",
   },
   {
     name: "Pens",
     url: "https://fogoplayer.github.io/pens",
     startDate: "Sept 18, 2020",
-    endDate: "Jun 6, 2021",
+    endDate: "Jan 31, 2023",
     lessons:
       "Bash Scripting, CSS Transform, CSS Transition, Google Apps Script, Bookmarklets",
     description:
-      "This is a collection of quick-and-dirty projects (bookmarklets and quick HTML files) that I've made over the years to learn new concepts or solve a particular problem. They're not supposed to be finished products--they're supposed to solve a very narrow problem."
-  }
+      "This is a collection of quick-and-dirty projects (bookmarklets and quick HTML files) that I've made over the years to learn new concepts or solve a particular problem. They're not supposed to be finished products--they're supposed to solve a very narrow problem.",
+  },
+  {
+    name: "Wikiguessr",
+    url: "wikiguessr.zarinloosli.com",
+    startDate: "July 30, 2022",
+    endDate: "PRESENT",
+    lessons: "Web Components, LitElement, RTC",
+    description: "A very nerdy party game built using web components.",
+  },
+  {
+    name: "Gravitate",
+    url: "beta.gravitate.zarinloosli.com",
+    startDate: "April 30, 2022",
+    endDate: "Dec 24,2022",
+    lessons: "Virtal DOM, htm, web components",
+    description:
+      "I had just learned about HTM and wanted to see how close I could get to a JSX-like DevEx without a build step. The result is... not great, with buggy reactivity, but I learned a lot!",
+  },
+  {
+    name: "Dynamic Tabs",
+    url: "https://fogoplayer.github.io/",
+    startDate: "Sep 5, 2022",
+    endDate: "PRESENT",
+    lessons: "Chrome Extensions, Ionic",
+    description:
+      "I have strong feelings about tab management, and browser UX in general. Dynamic Tabs started out as a progressive enhancement of Chrome Tab Groups, and has expanded to a full-on alternative interface for web browsers. It's still in the early stages of development, but I'm really excited about it!",
+  },
 ];
+
+// {
+//   name: "",
+//   url: "https://fogoplayer.github.io/",
+//   startDate: "",
+//   endDate: "PRESENT",
+//   lessons: "",
+//   description:
+//     "",
+// },
